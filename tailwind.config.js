c2f610446a9239b07147a1d8b0dc20f4fa3b3324
Micael Hernandes <micael@macedo.com>
2024-06-18 19:41:24 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#CF2028',
+        'secondary': '#FFB71B',
+        'danger': '#e3342f',
+      },
+    },
+  },
+  plugins: [],
+}
