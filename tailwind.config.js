f1b09a5199ec9b50d61537fcfb49bc54a5e23509
Micael Hernandes <micael@macedo.com>
2024-06-18 19:54:27 -0300

    style: novos breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens:{
+      'sm': '640px',
+      'md': '1000px',
+      'lg': '1200px',
+      'xl': '1480px',
+    },
     extend: {
       colors: {
         'primary': '#CF2028',
